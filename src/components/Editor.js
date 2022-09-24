0064f8199d0f754585f484490904a2b157170cf5
edwardzhang5 <edwardzhang5@gmail.com>
2022-09-24 16:50:32 -0400

    Change to dark mode

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import AceEditor from 'react-ace'
 
 import 'ace-builds/src-noconflict/mode-java'
-import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/theme-twilight'
 import 'ace-builds/src-noconflict/ext-language_tools'
 
 function onChange(newValue) {
@@ -14,7 +14,7 @@ export default class Editor extends Component {
     return (
       <AceEditor
         mode='java'
-        theme='github'
+        theme='twilight'
         onChange={onChange}
         name='UNIQUE_ID_OF_DIV'
         editorProps={{ $blockScrolling: true }}
