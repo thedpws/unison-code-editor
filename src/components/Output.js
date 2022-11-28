00dc6eeb2255705b3b1d391e3e926bbe414a7a0d
edwardzhang5 <edwardzhang5@gmail.com>
2022-11-28 18:24:50 -0500

    added testcase component

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import TestCase from './TestCase'
 
 const Output = (props) => {
-
-    return (
-        <div className = "relative m-6 flex flex-col">
-            <div className='text-lg font-semibold font-sans text-left'>
-                Output
-            </div>
-            <div className='mt-3 flex-1 overscroll-y-auto overflow-auto p-2 bg-gray-400'>
-                {props.text}
-            </div>
-        </div>
-    )
+  return (
+    <div className='relative m-6 flex flex-col'>
+      <div className='text-lg font-semibold font-sans text-left'>Output</div>
+      <div className='mt-3 flex-1 overscroll-y-auto overflow-auto p-2 bg-gray-400'>
+        {props.text}
+        <TestCase number={1} result={'Pass'} />
+        <TestCase number={2} result={'Fail'} />
+        <TestCase number={3} result={'Pass'} />
+        <TestCase number={4} result={'Pass'} />
+      </div>
+    </div>
+  )
 }
 export default Output
---
+++ src/components/TestCase.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const TestCase = (props) => {
+  return (
+    <div className='relative m-6 flex flex-col'>
+      <div className='text-lg font-semibold font-sans text-left'>
+        Test Case {props.number}
+      </div>
+      {props.result === 'Pass' ? (
+        <div className='mt-3 flex-1 overscroll-y-auto overflow-auto p-2 bg-green-400'>
+          {props.result}
+        </div>
+      ) : (
+        <div className='mt-3 flex-1 overscroll-y-auto overflow-auto p-2 bg-red-400'>
+          {props.result}
+        </div>
+      )}
+    </div>
+  )
+}
+export default TestCase
