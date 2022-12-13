18adf12d9ca8cc3f5edd0a495709f6e77f2bbfb2
Edward Zhang <60411447+edwardzhang5@users.noreply.github.com>
2022-12-13 10:48:09 -0500

    added std out, changed grid on output, created mapping for testcases

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -1,31 +1,31 @@
-import Split from 'react-split'
-import Editor from './Editor'
-import { useState } from 'react'
-import Instructions from './Instructions'
-import Output from './Output'
+import Split from "react-split";
+import Editor from "./Editor";
+import { useState } from "react";
+import Instructions from "./Instructions";
+import Output from "./Output";
 
 const Panels = (props) => {
-  const [collapsedIndex, setCollapsedIndex] = useState(null)
+  const [collapsedIndex, setCollapsedIndex] = useState(null);
 
   return (
     <Split
       collapsed={collapsedIndex}
-      direction='vertical'
+      direction="vertical"
       sizes={[60, 40]}
       style={{ height: `calc(100vh - 4rem)` }}
     >
-      <Split className='flex'>
-        <div className='bg-gray-300'>
-          <Instructions text={props.instructions} examples={props.examples}/>
+      <Split className="flex">
+        <div className="bg-gray-300">
+          <Instructions text={props.instructions} examples={props.examples} />
         </div>
-        <div className='bg-gray-300'>
+        <div className="bg-gray-300">
           <Editor setEditorValue={props.setEditorValue} />
         </div>
       </Split>
-      <div className='bg-gray-400 flex'>
-        <Output text={props.outputText} />
+      <div className="bg-gray-400 flex">
+        <Output testCases={props.testCases} />
       </div>
     </Split>
-  )
-}
-export default Panels
+  );
+};
+export default Panels;
