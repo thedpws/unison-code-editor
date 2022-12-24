f0dcc3bbf43ca38ec63285f7fdcc7bd906918fb4
Adeeb Rashid <adeeb.rashid@ufl.edu>
2022-12-24 15:31:26 -0500

    fixed button positioning

--- src/components/Output.js
+++ src/components/Output.js
@@ -6,10 +6,10 @@ import { AiFillPlayCircle } from "react-icons/ai";
 const Output = (props) => {
   console.log(props.testCases)
   return (
-    <div className="relative m-6 flex flex-col">
+    <div className="relative m-6 flex flex-col" style={{ width: '100%' }}>
       <div className='grid grid-cols-8 gap-4'>
         <div className="text-lg font-semibold font-sans text-left">Output</div>
-        <div className='col-start-5 col-span-2'>
+        <div className='col-start-7'>
           <button
             type="button"
             className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
@@ -18,20 +18,24 @@ const Output = (props) => {
             Run Code
           </button>
         </div>
-        <div className='col-end-8'>
+        <div className='col-start-8'>
           <button
             type="button"
-            className="bg-blue-500 flex items-center gap-2 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
             onClick={props.getMockTestResults}
           >
-            <div>
-              Submit
+            <div className="flex items-center gap-2 ">
+              <div>
+                Submit
+              </div>
+              <AiFillPlayCircle />
             </div>
-            <AiFillPlayCircle />
           </button>
         </div>
       </div>
-      <div className='mt-4 flex-1 overscroll-y-auto overflow-auto'>
+      {/* To put the scroll bar on the right */}
+      {/* <div className='mt-4 flex-1 overscroll-y-auto overflow-auto'> */}
+      <div className='mt-4 flex overscroll-y-auto overflow-auto'>
         <TestCaseContainer testCases={props.testCases} />
       </div>
     </div>
--- src/components/TestCaseContainer.js
+++ src/components/TestCaseContainer.js
@@ -3,25 +3,7 @@ import { useState } from "react";
 const TestCaseContainer = (props) => {
     const [selectedCase, setSelectedCase] = useState(1);
     return props.testCases ? (
-        <div className='flex space-y-0'>
-            {/*Only Scroll on the Test Cases*/}
-            {/* <div className='bg-gray-700  p-2 flex-1 overscroll-y-auto overflow-auto'>
-                <div className="bg-gray-400">
-                    {props.testCases?.map((testcase) => {
-                        return (
-                            <TestCase
-                                key={testcase.key}
-                                number={testcase.number}
-                                result={testcase.result}
-                                stdout={testcase?.stdout}
-                                setSelectedCase={setSelectedCase}
-                            />
-                        );
-                    })}
-
-                </div>
-            </div> */}
-
+        <div className='flex' >
             {/*Create Grid*/}
             <div className='grid grid-cols-8 bg-gray-300 '>
                 <div className='bg-gray-700 col-span-2 p-2 flex-1 overscroll-y-auto overflow-auto'>
