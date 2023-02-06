37479c5fbe20d6a03b483ba5f5b5b564aeeb429a
Adeeb Rashid <adeeb.rashid@ufl.edu>
2023-02-06 12:04:55 -0500

    Reset Code button

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,11 @@ const Header = (props) => {
                       className={classNames(
                         active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                         "block px-4 py-2 text-sm"
-                      )}
+                      )
+                    }
+                    onClick={() => {
+                      props.setEditorValue(props.placeholders[props.currentLanguage])
+                    }}
                     >
                       Reset Code
                     </div>
